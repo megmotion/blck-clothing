b3cb72a5146886e42f56bdca2f5cf9aafcccf025
megmotion <33497517+megmotion@users.noreply.github.com>
2019-07-08 15:55:15 +0200

    adding redirect to home when signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser  }) => (
+const Header = ({ currentUser }) => (
 	<div className='header'>
 		<Link className='logo-container'  to="/">
 			<Logo className='logo'/>
